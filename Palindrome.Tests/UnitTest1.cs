daebb47cd9b597f0667de3a867fe9e16e4ecd168
Juan Davila <juandavil1101@gmail.com>
2021-09-28 20:18:00 -0500

    Add project files.

---
+++ Palindrome.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+
+namespace Palindrome.Tests
+{
+  public class PalindromeFinderTest
+  {
+    [Theory]
+    [InlineData("Stars", false)]
+    [InlineData("O, a kak Uwakov lil vo kawu kakao!", true)]
+    [InlineData("Some men interpret nine memos", true)]
+    [InlineData("red rum, sir, is murder", true)]
+    [InlineData("no lemon, no melon", true)]
+    [InlineData("never odd or even", true)]
+    public void Determine_if_the_string_passed_is_a_palindrome(string palindromeString, bool expectedPalindromeResult)
+    {
+      var palindromeFinder = new PalindromeFinder();
+      var isStringPalindrome = palindromeFinder.IsStringAPalindrome(palindromeString);
+      Assert.Equal(isStringPalindrome, expectedPalindromeResult);
+    }
+  }
+}
---
+++ Palindromes/PalindromeFinder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+  public class PalindromeFinder
+  {
+    public bool IsStringAPalindrome(string palindromeString)
+    {
+      var listOfCharacterToOverlook = new List<char> {' ', ',', '\'', '!', '?', '.', ';', ':', '\"', '/', '\\', '-'};
+      palindromeString = palindromeString.ToLower();
+
+      for (int i = 0, x = palindromeString.Length - 1; i <= x; i++, x--)
+      {
+        while (listOfCharacterToOverlook.Contains(palindromeString[i])) {i++;}
+        while (listOfCharacterToOverlook.Contains(palindromeString[x])) {x--;}
+        if (palindromeString[i] != palindromeString[x]) return false;
+      }
+      return true;
+    }
+  }
+}
